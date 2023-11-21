eebac85fddcabfc69917ad59686c2d385de4726d
João Choma Neto <jchomax@gmail.com>
2023-11-21 14:35:05 -0300

    criei a regra de negocio de adicionar tarefas

--- projeto/scripts.js
+++ projeto/scripts.js
@@ -1,3 +1,20 @@
-function somar(x,y){
-    return x+y;
+var tarefas = [];
+
+var tarefa = document.getElementById("tarefa");
+var minhaLista = document.getElementById("minhalista");
+
+function registrar(){   
+    tarefas.push(tarefa);
+    imprimir();
 }
+
+function imprimir(){
+    minhaLista.innerHTML = "";
+    for(var i = 0; i < tarefas.length; i++){
+        var li = document.createElement('li');
+        li.textContent = tarefas[i];
+        minhaLista.appendChild(li);
+    }
+}
+
+imprimir();
